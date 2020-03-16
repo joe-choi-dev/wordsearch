3a3831882f715270fac5f95b55055c4751f9e8ea
Joe Choi <joechoi@joes-mbp.myfiosgateway.com>
2020-03-16 15:57:10 -0400

    adding mouse events

--- src/GridContent.js
+++ src/GridContent.js
@@ -34,10 +34,81 @@ const styles = theme => ({
   
 class GridContent extends React.Component {  
 
+  constructor(props) {
+    super(props);
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseUpOut = this.onMouseUpOut.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+  }
+
   componentDidMount() {
     // this.updateCanvas();
     this.drawGrid();
     // this.drawBoard();
+  }
+
+  //turn on the coloring hints
+  onMouseDown(e){
+    // tell the browser we're handling this event
+    e.preventDefault();
+    e.stopPropagation();
+
+    //offset because of the margins
+    const BB = this.refs.canvas.getBoundingClientRect();
+    const offsetX=BB.left; 
+    const offsetY=BB.top; 
+  
+    const startX=parseInt(e.clientX-offsetX);
+    const startY=parseInt(e.clientY-offsetY);
+
+    // console.log({ offsetX: offsetX, offsetY: offsetY });
+    // console.log({ clientX: e.clientX, clientY: e.clientY });
+
+    // console.log({ startX: startX, startY: startY });
+  
+    // Put your mousedown stuff here
+    const isDown=true;
+  }
+
+  //turn on the coloring hints
+  onMouseMove(e) {
+    //if (isMouseDown) //then its dragging
+    // tell the browser we're handling this event
+    e.preventDefault();
+    e.stopPropagation();
+
+    //offset because of the margins
+    const BB = this.refs.canvas.getBoundingClientRect();
+    const offsetX=BB.left; 
+    const offsetY=BB.top; 
+  
+    const startX=parseInt(e.clientX-offsetX);
+    const startY=parseInt(e.clientY-offsetY);
+
+    // console.log({ offsetX: offsetX, offsetY: offsetY });
+    // console.log({ clientX: e.clientX, clientY: e.clientY });
+
+    // console.log({ moveX: startX, moveY: startY });
+  }
+
+  //verify if its correct
+  onMouseUpOut(e){
+    // Put your mouseup stuff here
+    const isDown=false;
+
+    //offset because of the margins
+    const BB = this.refs.canvas.getBoundingClientRect();
+    const offsetX=BB.left; 
+    const offsetY=BB.top; 
+  
+    // tell the browser we're handling this event
+    e.preventDefault();
+    e.stopPropagation();
+  
+    const endX=parseInt(e.clientX-offsetX);
+    const endY=parseInt(e.clientY-offsetY);
+  
+    // console.log({ endX: endX, endY: endY });
   }
 
   drawGrid() {
@@ -48,9 +119,9 @@ class GridContent extends React.Component {
     var cellHeight=40;
 
     var letters = ['g', 'b', 's', 'i', 'c', 'e', 'n', 'o', 'b' ,'a']
-      .concat(['g', 'b', 's', 'i', 'c', 'e', 'n', 'o', 'b' ,'a']) 
-      .concat(['g', 'b', 's', 'i', 'c', 'e', 'n', 'o', 'b' ,'a']) 
-      .concat(['g', 'b', 's', 'i', 'c', 'e', 'n', 'o', 'b' ,'a']) 
+      .concat(['o', 'b', 's', 'i', 'c', 'e', 'n', 'o', 'b' ,'a']) 
+      .concat(['o', 'b', 's', 'i', 'c', 'e', 'n', 'o', 'b' ,'a']) 
+      .concat(['d', 'b', 's', 'i', 'c', 'e', 'n', 'o', 'b' ,'a']) 
       .concat(['g', 'b', 's', 'i', 'c', 'e', 'n', 'o', 'b' ,'a']) 
       .concat(['g', 'b', 's', 'i', 'c', 'e', 'n', 'o', 'b' ,'a']) 
       .concat(['g', 'b', 's', 'i', 'c', 'e', 'n', 'o', 'b' ,'a']) 
@@ -67,40 +138,44 @@ class GridContent extends React.Component {
 
     ctx.fillStyle='black';
     for(var i=0;i<letters.length;i++){
-      var row=parseInt(i/cols);
+      var row=parseInt(i/cols); //rowNumber
       var col=i-row*cols;
-      console.log(row);
-      console.log(cols);
-      console.log(col*cellWidth+cellWidth/2);
-      console.log(row*cellHeight+cellHeight/2);
-      console.log(" ");
+      // console.log(row);
+      // console.log(cols);
+      // console.log(col*cellWidth+cellWidth/2);
+      // console.log(row*cellHeight+cellHeight/2);
+      // console.log(" ");
       ctx.fillText(letters[i], col*cellWidth+cellWidth/2, row*cellHeight+cellHeight/2); //20,20 -> 20,380
     }
   }
 
-  drawOverlay() {
-  }
-
   updateCanvas() {
     const ctx = this.refs.canvas.getContext('2d');
-    ctx.lineWidth = 20;
-    ctx.lineCap = 'round';
+    ctx.lineCap = "round";
+    ctx.lineWidth=20;
+    ctx.font='14px Roboto';
+    ctx.textAlign='center';
+    ctx.textBaseline='middle';
     ctx.globalAlpha= 0.25; 
-    ctx.moveTo(27, 27);
-    ctx.lineTo(27, 175);
+    ctx.moveTo(20, 20);
+    ctx.lineTo(20, 380);
     ctx.stroke();
   }
+
 
   render() {
     const { classes }  = this.props;
     return (
       <div className={classes.contentHolder}>
         <div className={classes.canvasWrap}>
-        {/* <canvas className={classes.overlay} ref="c" width={421} height={421}/> */}
-        <canvas className={classes.canvas} ref="canvas" width={400} height={400}/>
-        {/* <div className={classes.content}>
-          <GridRow key={0} id={0} content={['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']}/>
-        </div> */}
+        <canvas className={classes.canvas} 
+          onMouseDown={this.onMouseDown}
+          onMouseUp={this.onMouseUpOut}
+          onMouseOut={this.onMouseUpOut}
+          onMouseMove={this.onMouseMove}
+          ref="canvas" 
+          width={400} 
+          height={400}/>
       </div>
       </div>
     );
@@ -108,30 +183,3 @@ class GridContent extends React.Component {
 }
 
 export default withStyles(styles)(GridContent);
-
-
-//Scratch
-// drawBoard(){
-//   //grid width and height
-//   var bw = 400;
-//   var bh = 400;
-//   //padding around grid
-//   var p = 10;
-//   //size of canvas
-//   var cw = bw + (p*2) + 1;
-//   var ch = bh + (p*2) + 1;
-
-//   const context = this.refs.c.getContext('2d');
-//   for (var x = 0; x <= bw; x += 40) {
-//       context.moveTo(0.5 + x + p, p);
-//       context.lineTo(0.5 + x + p, bh + p);
-//   }
-
-//   for (var x = 0; x <= bh; x += 40) {
-//       context.moveTo(p, 0.5 + x + p);
-//       context.lineTo(bw + p, 0.5 + x + p);
-//   }
-
-//   context.strokeStyle = "black";
-//   context.stroke();
-// }
